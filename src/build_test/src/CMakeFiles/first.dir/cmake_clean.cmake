e71d7d6f46ca128d698e5dc1cfe46076a9ca9236
Rob Lim <robert.m.lim@gmail.com>
2025-06-27 00:47:22 +0000

    Work through 1.7

---
+++ src/build_test/src/CMakeFiles/first.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first.dir/first.cpp.o"
+  "libfirst.a"
+  "libfirst.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
