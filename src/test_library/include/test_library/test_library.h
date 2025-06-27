3d838f19c2e474f49bc9c74a2d719d9d6f796351
Rob Lim <robert.m.lim@gmail.com>
2025-06-27 01:08:25 +0000

    Add test_library

---
+++ src/test_library/include/test_library/test_library.h
@@ -0,0 +1,8 @@
+#ifndef TEST_LIBRARY_H
+#define TEST_LIBRARY_H
+
+#include <ros/ros.h>
+
+void display_pos(float x, float y);
+
+#endif
---
+++ src/test_library/src/test_library.cpp
@@ -0,0 +1,9 @@
+#include "../include/test_library/test_library.h"
+
+void display_pos(float x, float y) {
+     
+    float pos_x = x;
+    float pos_y = y;
+    std::cout << "Position X: " << pos_x << " Position Y: " << pos_y << std::endl;
+     
+}
